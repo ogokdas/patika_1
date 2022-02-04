f1063b208e0de7c8d860bb07a1dc1d900bd219fc
ogokdas <ogokdas@yandex.com>
2022-02-04 14:33:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello World";
+            Console.WriteLine(input);
+
+            Console.Write("Lütfen birşeyler yazın:");
+            string input2 = Console.ReadLine();
+            Console.WriteLine("Input: {0}",input2);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
